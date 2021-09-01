a9d64efce4ed1fe84f0c94a6dd1cbf5c6695b3b9
lee.huimok <hellowin6076@gmail.com>
2021-09-02 00:30:38 +0900

    Signup User

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,16 +1,23 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor //final필드를 DI할때 사용
 @Controller//1.IoC 2.파일을 리턴하는 컨트롤러
 public class AuthController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final AuthService authService;
 
     @GetMapping("/auth/signin")
     public String signinForm(){
@@ -25,6 +32,11 @@ public class AuthController {
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto){ //key=value (x-www-form-urlencoded)
         log.info(signupDto.toString());
+        //User <- SignupDto
+        User user = signupDto.toEntity();
+        log.info(user.toString());
+        User userEntity =  authService.signup(user);
+        System.out.println(userEntity);
         return "auth/signin";
     }
 }
