837c4a9d7bb3ac35eeb3e850d959c3709b4d8cde
lee.huimok <hellowin6076@gmail.com>
2021-09-01 22:54:20 +0900

    Create User Model

---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,30 @@
+package com.cos.photogramstart.web;
+
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller//1.IoC 2.파일을 리턴하는 컨트롤러
+public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    @GetMapping("/auth/signin")
+    public String signinForm(){
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm(){
+        return "auth/signup";
+    }
+
+    @PostMapping("/auth/signup")
+    public String signup(SignupDto signupDto){ //key=value (x-www-form-urlencoded)
+        log.info(signupDto.toString());
+        return "auth/signin";
+    }
+}
